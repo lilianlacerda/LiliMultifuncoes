d36f3f105c6dad7eb374613d055673af4a70850e
lilianlacerda <lilianraiany@gmail.com>
2025-01-03 16:01:31 -0300

    add: README

---
+++ README.mk
@@ -0,0 +1,13 @@
+
+# Bem vindo ao Lili Multifunções!
+
+Aqui você encontra algumas funcionalidades que aprendi ao longo dos meus estudos independentes de JavaScript, Lógica de programação e HTML com CSS! 
+
+## O que você vai encontrar por aqui
+
+- Dado virtual;
+- Calculo de conversão de Celcius para fahrenheit e o contrário também :wink:
+- Calculo de média. 
+
+Me dedico a aperfeiçoar cada dia mais e inserir novas funcionalidades a esse site!
+
